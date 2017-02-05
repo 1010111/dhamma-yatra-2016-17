7f93d53437d459f7ee1df5c55b1809f599d66d65
1010111 <1010111chen@gmail.com>
2017-02-05 16:33:58 +0700

    debugging

--- js/main.js
+++ js/main.js
@@ -28,6 +28,7 @@ jQuery(document).ready(function($){
 	
 	$.getJSON('js/itinerary.json', function(data){
 		$.each( data, function( key, val ) {
+			console.log('key:'+key+', val:'+val);
 			itineraries.push(val);
 		});
 	}).done(function(){
